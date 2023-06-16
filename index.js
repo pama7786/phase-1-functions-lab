a7e056c393bf4dca788f5b55465804d25cddc8b3
Abdirahman abass diis <abdirahmanfarmajo7786@gmail.com>
2023-06-16 15:28:43 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42;
+    return Math.abs(pickupLocation - hqLocation);
+  }
+
+function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    const feetPerBlock = 264;
+  
+    return blocks * feetPerBlock;
+  }
+
+function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(start - destination);
+    const feetPerBlock = 264;
+  
+    return blocks * feetPerBlock;
+  }
+  
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return "cannot travel that far";
+    }
+  }
+  
+ module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice,
+  };
+  
